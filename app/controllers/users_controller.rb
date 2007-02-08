857fbb88ba75b41e804445d9f49238b87ba4ce7f
dcparker <dcparker@55dd9f42-8528-0410-b227-b9e89acb4f78>
2007-02-08 21:25:17 +0000

    Generated Authentication with activation mailer
    git-svn-id: http://store.sabretechllc.com/public/sixsigma/hipforms/trunk@10 55dd9f42-8528-0410-b227-b9e89acb4f78

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+  # Be sure to include AuthenticationSystem in Application Controller instead
+  include AuthenticatedSystem
+  # If you want "remember me" functionality, add this before_filter to Application Controller
+  before_filter :login_from_cookie
+
+  # render new.rhtml
+  def new
+  end
+
+  def create
+    @user = User.new(params[:user])
+    @user.save!
+    self.current_user = @user
+    redirect_back_or_default('/')
+    flash[:notice] = "Thanks for signing up!"
+  rescue ActiveRecord::RecordInvalid
+    render :action => 'new'
+  end
+
+  def activate
+    self.current_user = User.find_by_activation_code(params[:activation_code])
+    if logged_in? && !current_user.activated?
+      current_user.activate
+      flash[:notice] = "Signup complete!"
+    end
+    redirect_back_or_default('/')
+  end
+
+end
