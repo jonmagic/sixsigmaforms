cc606fc75b03d85cd64f8b8b41b911052b9156f6
jonmagic <jonmagic@55dd9f42-8528-0410-b227-b9e89acb4f78>
2007-03-01 19:12:35 +0000

    attempting to split out some admin functions to a new spot
    git-svn-id: http://store.sabretechllc.com/public/sixsigma/modelA@55 55dd9f42-8528-0410-b227-b9e89acb4f78

---
+++ app/controllers/admin/pages_controller.rb
@@ -0,0 +1,82 @@
+class PagesController < ApplicationController
+  before_filter :require_ssadmin_except_for_show
+  layout 'admin'
+
+  # GET /pages
+  # GET /pages.xml
+  def index #This is the ACTION 'index', accessed via /ssadmin/pages, as opposed to the STUB 'index', accessed via /pages
+    @pages = Page.find(:all)
+
+    respond_to do |format|
+      format.html # index.rhtml
+      format.xml  { render :xml => @pages.to_xml }
+    end
+  end
+
+  # GET /pages/1
+  # GET /pages/1.xml
+  def show
+    @page = Page.find_by_stub(params[:stub]) || Page.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.rhtml
+      format.xml  { render :xml => @page.to_xml }
+    end
+  end
+
+  # GET /pages/new
+  def new
+    @page = Page.new
+  end
+
+  # GET /pages/1;edit
+  def edit
+    @page = Page.find_by_stub(params[:stub]) || Page.find(params[:id])
+  end
+
+  # POST /pages
+  # POST /pages.xml
+  def create
+    @page = Page.new(params[:page])
+
+    respond_to do |format|
+      if @page.save
+        flash[:notice] = 'Page was successfully created.'
+        format.html { redirect_to page_url(@page) }
+        format.xml  { head :created, :location => page_url(@page) }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @page.errors.to_xml }
+      end
+    end
+  end
+
+  # PUT /pages/1
+  # PUT /pages/1.xml
+  def update
+    @page = Page.find_by_stub(params[:stub]) || Page.find(params[:id])
+
+    respond_to do |format|
+      if @page.update_attributes(params[:page])
+        flash[:notice] = 'Page was successfully updated.'
+        format.html { redirect_to page_url(@page) }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @page.errors.to_xml }
+      end
+    end
+  end
+
+  # DELETE /pages/1
+  # DELETE /pages/1.xml
+  def destroy
+    @page = Page.find_by_stub(params[:stub]) || Page.find(params[:id])
+    @page.destroy
+
+    respond_to do |format|
+      format.html { redirect_to pages_url }
+      format.xml  { head :ok }
+    end
+  end
+end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,17 +1,5 @@
 class PagesController < ApplicationController
-  layout 'pages'
   before_filter :require_ssadmin_except_for_show
-
-  # GET /pages
-  # GET /pages.xml
-  def index #This is the ACTION 'index', accessed via /ssadmin/pages, as opposed to the STUB 'index', accessed via /pages
-    @pages = Page.find(:all)
-
-    respond_to do |format|
-      format.html # index.rhtml
-      format.xml  { render :xml => @pages.to_xml }
-    end
-  end
 
   # GET /pages/1
   # GET /pages/1.xml
@@ -24,59 +12,4 @@ class PagesController < ApplicationController
     end
   end
 
-  # GET /pages/new
-  def new
-    @page = Page.new
-  end
-
-  # GET /pages/1;edit
-  def edit
-    @page = Page.find_by_stub(params[:stub]) || Page.find(params[:id])
-  end
-
-  # POST /pages
-  # POST /pages.xml
-  def create
-    @page = Page.new(params[:page])
-
-    respond_to do |format|
-      if @page.save
-        flash[:notice] = 'Page was successfully created.'
-        format.html { redirect_to page_url(@page) }
-        format.xml  { head :created, :location => page_url(@page) }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @page.errors.to_xml }
-      end
-    end
-  end
-
-  # PUT /pages/1
-  # PUT /pages/1.xml
-  def update
-    @page = Page.find_by_stub(params[:stub]) || Page.find(params[:id])
-
-    respond_to do |format|
-      if @page.update_attributes(params[:page])
-        flash[:notice] = 'Page was successfully updated.'
-        format.html { redirect_to page_url(@page) }
-        format.xml  { head :ok }
-      else
-        format.html { render :action => "edit" }
-        format.xml  { render :xml => @page.errors.to_xml }
-      end
-    end
-  end
-
-  # DELETE /pages/1
-  # DELETE /pages/1.xml
-  def destroy
-    @page = Page.find_by_stub(params[:stub]) || Page.find(params[:id])
-    @page.destroy
-
-    respond_to do |format|
-      format.html { redirect_to pages_url }
-      format.xml  { head :ok }
-    end
-  end
 end
